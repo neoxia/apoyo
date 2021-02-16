3bc8aa51736495da68602d72ee0a3be0c49314b6
Sascha Braun <sascha.braun245@gmail.com>
2021-02-16 20:44:39 +0100

    feat(results): rename functions + add unit tests

--- src/Result.ts
+++ src/Result.ts
@@ -2,33 +2,40 @@ import { identity, pipe, throwError } from './function'
 import { isSome, Option } from './Option'
 import { isObject } from './types'
 
-export interface Ok<T> {
-  _tag: 'Result.Ok'
-  ok: T
-}
-export interface Ko<T> {
-  _tag: 'Result.Ko'
-  ko: T
+const enum Tags {
+  Ok = 'Result.Ok',
+  Ko = 'Result.Ko'
 }
 
-export type Result<A, E = never> = Ok<A> | Ko<E>
+export namespace Result {
+  export interface Ok<T> {
+    _tag: Tags.Ok
+    ok: T
+  }
+  export interface Ko<T> {
+    _tag: Tags.Ko
+    ko: T
+  }
+}
 
-export const ok = <T>(value: T): Ok<T> => ({
-  _tag: 'Result.Ok',
+export type Result<A, E = never> = Result.Ok<A> | Result.Ko<E>
+
+export const ok = <T>(value: T): Result.Ok<T> => ({
+  _tag: Tags.Ok,
   ok: value
 })
-export const ko = <T>(value: T): Ko<T> => ({
-  _tag: 'Result.Ko',
+export const ko = <T>(value: T): Result.Ko<T> => ({
+  _tag: Tags.Ko,
   ko: value
 })
 
 export const fromOption = <E = unknown>(onNone: () => E) => <A>(option: Option<A>): Result<A, E> =>
   isSome(option) ? ok(option) : ko(onNone())
 
-export const isOk = <A, B>(result: Result<A, B>): result is Ok<A> => result._tag === 'Result.Ok'
-export const isKo = <A, B>(result: Result<A, B>): result is Ko<B> => result._tag === 'Result.Ko'
+export const isOk = <A, B>(result: Result<A, B>): result is Result.Ok<A> => result._tag === Tags.Ok
+export const isKo = <A, B>(result: Result<A, B>): result is Result.Ko<B> => result._tag === Tags.Ko
 export const isResult = <A = unknown, B = unknown>(result: unknown): result is Result<A, B> =>
-  isObject(result) && (result._tag === 'Result.Ok' || result._tag === 'Result.Ko')
+  isObject(result) && (result._tag === Tags.Ok || result._tag === Tags.Ko)
 
 export const get = <A, E = unknown>(result: Result<A, E>) => (isOk(result) ? result.ok : throwError(result.ko))
 
@@ -43,7 +50,7 @@ export const join = <A, E>(result: Result<Result<A, E>, E>): Result<A, E> => pip
 export const chain = <A, B, E = unknown>(fn: (value: A) => Result<B, E>) => (result: Result<A, E>): Result<B, E> =>
   isOk(result) ? fn(result.ok) : result
 
-export const alt = <A, E = unknown>(fn: (err: E) => Result<A, E>) => (result: Result<A, E>): Result<A, E> =>
+export const catchError = <A, E = unknown>(fn: (err: E) => Result<A, E>) => (result: Result<A, E>): Result<A, E> =>
   isKo(result) ? fn(result.ko) : result
 
 export const fold = <R, A, E = unknown>(onOk: (value: A) => R, onKo: (value: E) => R) => (result: Result<A, E>) => {
@@ -72,13 +79,14 @@ export const Result = {
   ko,
   isOk,
   isKo,
+  isResult,
   fromOption,
   get,
   map,
   mapError,
   join,
   chain,
-  alt,
+  catchError,
   fold,
   swap,
   tryCatch
---
+++ tests/Result.ts
@@ -0,0 +1,218 @@
+import { pipe, Result } from '../src'
+
+describe('Result.ok', () => {
+  it('should create ok', () => {
+    const v = Result.ok(1)
+    expect(v).toEqual({
+      _tag: 'Result.Ok',
+      ok: 1
+    })
+    expect(v).toEqual(Result.ok(1))
+  })
+})
+
+describe('Result.ko', () => {
+  it('should create ko', () => {
+    const v = Result.ko(1)
+    expect(v).toEqual({
+      _tag: 'Result.Ko',
+      ko: 1
+    })
+    expect(v).toEqual(Result.ko(1))
+  })
+})
+
+describe('Result.fromOption', () => {
+  it('should create ok from some value', () => {
+    const v = pipe(
+      0,
+      Result.fromOption(() => 1)
+    )
+    expect(v).toEqual(Result.ok(0))
+  })
+  it('should create ko from undefined', () => {
+    const v = pipe(
+      undefined,
+      Result.fromOption(() => 1)
+    )
+    expect(v).toEqual(Result.ko(1))
+  })
+})
+
+describe('Result.isOk', () => {
+  it('should return true', () => {
+    expect(pipe(Result.ok(1), Result.isOk)).toBe(true)
+  })
+
+  it('should return false for ko', () => {
+    expect(pipe(Result.ko(1), Result.isOk)).toBe(false)
+  })
+})
+
+describe('Result.isKo', () => {
+  it('should return true', () => {
+    expect(pipe(Result.ko(1), Result.isKo)).toBe(true)
+  })
+
+  it('should return false for ok', () => {
+    expect(pipe(Result.ok(1), Result.isKo)).toBe(false)
+  })
+})
+
+describe('Result.isResult', () => {
+  it('should return true', () => {
+    expect(pipe(Result.ko(1), Result.isResult)).toBe(true)
+    expect(pipe(Result.ok(1), Result.isResult)).toBe(true)
+  })
+
+  it('should return false for unknown object', () => {
+    expect(pipe({ name: 'John Doe' }, Result.isResult)).toBe(false)
+  })
+})
+
+describe('Result.get', () => {
+  it('should return expected results', () => {
+    expect(pipe(Result.ok(1), Result.get)).toBe(1)
+  })
+
+  it('should throw on Ko', () => {
+    expect(() => pipe(Result.ko('on ko'), Result.get)).toThrow('on ko')
+  })
+})
+
+describe('Result.map', () => {
+  it('should execute on ok', () => {
+    const res = pipe(
+      Result.ok(1),
+      Result.map((a) => a + 1)
+    )
+    expect(res).toEqual(Result.ok(2))
+  })
+
+  it('should not execute on ko', () => {
+    const res = pipe(
+      Result.ko(1),
+      Result.map((a: any) => a + 1)
+    )
+    expect(res).toEqual(Result.ko(1))
+  })
+})
+
+describe('Result.mapError', () => {
+  it('should execute on ko', () => {
+    const res = pipe(
+      Result.ko(1),
+      Result.mapError((a) => a + 1)
+    )
+    expect(res).toEqual(Result.ko(2))
+  })
+
+  it('should not execute on ok', () => {
+    const res = pipe(
+      Result.ok(1),
+      Result.mapError((a: number) => a + 1)
+    )
+    expect(res).toEqual(Result.ok(1))
+  })
+})
+
+describe('Result.chain', () => {
+  it('should execute on ok', () => {
+    const res = pipe(
+      Result.ok(1),
+      Result.chain((a) => Result.ok(a + 1))
+    )
+    expect(res).toEqual(Result.ok(2))
+  })
+
+  it('can chain error', () => {
+    const res = pipe(
+      Result.ok(1),
+      Result.chain((a) => Result.ko(a + 1))
+    )
+    expect(res).toEqual(Result.ko(2))
+  })
+
+  it('should not execute on ko', () => {
+    const res = pipe(
+      Result.ko(1),
+      Result.chain((a: number) => Result.ok(a + 1))
+    )
+    expect(res).toEqual(Result.ko(1))
+  })
+})
+
+describe('Result.catchError', () => {
+  it('should execute on ko', () => {
+    const res = pipe(
+      Result.ko(1),
+      Result.catchError((a) => Result.ko(a + 1))
+    )
+    expect(res).toEqual(Result.ko(2))
+  })
+
+  it('can chain ok', () => {
+    const res = pipe(
+      Result.ko(1),
+      Result.catchError((a: number) => Result.ok(a + 1))
+    )
+    expect(res).toEqual(Result.ok(2))
+  })
+
+  it('should not execute on ok', () => {
+    const res = pipe(
+      Result.ok(1),
+      Result.catchError((a: any) => Result.ko(a + 1))
+    )
+    expect(res).toEqual(Result.ok(1))
+  })
+})
+
+describe('Result.swap', () => {
+  it('should swap ok to ko', () => {
+    const v = pipe(Result.ok(1), Result.swap)
+    expect(v).toEqual(Result.ko(1))
+  })
+  it('should swap ko to ok', () => {
+    const v = pipe(Result.ko(1), Result.swap)
+    expect(v).toEqual(Result.ok(1))
+  })
+})
+
+describe('Result.fold', () => {
+  it('should exec ok callback', () => {
+    const v = pipe(
+      Result.ok(1),
+      Result.fold(
+        (value) => value + 1,
+        () => 0
+      )
+    )
+    expect(v).toEqual(2)
+  })
+  it('should exec ko callback', () => {
+    const v = pipe(
+      Result.ko(1),
+      Result.fold(
+        (value: number) => value + 1,
+        () => 0
+      )
+    )
+    expect(v).toEqual(0)
+  })
+})
+
+describe('Result.tryCatch', () => {
+  it('should set result to ok', () => {
+    const v = pipe(() => {
+      return 1
+    }, Result.tryCatch)
+    expect(v).toEqual(Result.ok(1))
+  })
+  it('should catch error to ko', () => {
+    const v = pipe(() => {
+      throw 1
+    }, Result.tryCatch)
+    expect(v).toEqual(Result.ko(1))
+  })
+})
