9e8460c38739479a94c7d160a13316b145a33ff3
Sascha Braun <sascha.braun245@gmail.com>
2021-05-11 21:09:17 +0200

    docs(decoders): add tsdocs for Decoder module

--- packages/decoders/src/Decoder.ts
+++ packages/decoders/src/Decoder.ts
@@ -101,17 +101,329 @@ export function union<I>(
 
 export const unknown: Decoder<unknown, unknown> = Result.ok
 
+/**
+ * @namespace Decoder
+ *
+ * @description
+ *
+ * A `Decoder` is a function, that from an input I to create an output O, or produce an DecodeError.
+ * As such, a common use-case for `Decoder`s are type and value validations.
+ *
+ * This namespace contains the core utilities to:
+ * - Create `Decoder`s
+ * - Use or combine them
+ * - Extract / infer the resulting type informations
+ *
+ * @example
+ * ```ts
+ * export const TodoDto = ObjectDecoder.struct({
+ *   id: IntegerDecoder.int,
+ *   title: TextDecoder.range(1, 100),
+ *   description: pipe(TextDecoder.range(0, 2000), TextDecoder.nullable),
+ *   done: BooleanDecoder.boolean
+ * })
+ *
+ * export interface TodoDto extends Decoder.TypeOf<typeof TodoDto> {}
+ * ```
+ */
 export const Decoder = {
+  /**
+   * @description
+   * Creates a new decoder from a type guard
+   *
+   * @example
+   * ```ts
+   * const stringDecoder = Decoder.fromGuard(
+   *   (input: unknown): input is string => typeof input === 'string',
+   *   'value is not a string'
+   * )
+   *
+   * expect(pipe('Hello', Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe(42, Decoder.validate(decoder), Result.isKo)).toBe(true)
+   * ```
+   */
   fromGuard,
+
+  /**
+   * @description
+   * Map over the resulting value of an `Decoder`
+   *
+   * @example
+   * ```ts
+   * const decoder = pipe(
+   *   TextDecoder.string,
+   *   Decoder.map(str => str.trim())
+   * )
+   *
+   * expect(pipe('  Hello  ', Decoder.validate(decoder), Result.get)).toBe('Hello')
+   * ```
+   */
   map,
+
+  /**
+   * @description
+   * Add a custom validation function to an `Decoder`.
+   *
+   * Compared to `Decoder.guard`, this function gives more control and allows the user to modify the resulting value.
+   *
+   * @see `Decoder.guard`
+   * @see `Decoder.filter`
+   * @see `Decoder.reject`
+   *
+   * @example
+   * ```ts
+   * const validateAge = (dob: string): Result<string, DecodeError> => {
+   *   const now = new Date()
+   *   const date = new Date(dob)
+   *
+   *   if (date.getFullYear() < now.getFullYear() - 100) {
+   *     return Result.ko(DecodeError.value(dob, 'Date of birth is more than 100 years ago'))
+   *   }
+   *   if (date.getFullYear() > now.getFullYear() - 18) {
+   *     return Result.ko(DecodeError.value(dob, 'Date of birth is less than 18 years ago'))
+   *   }
+   *   return Result.ok(dob)
+   * }
+   *
+   * const birthdayDecoder = pipe(
+   *   DateDecoder.date,
+   *   Decoder.parse(validateAge)
+   * )
+   *
+   * expect(pipe('1930-01-01', Decoder.validate(birthdayDecoder), Result.isOk)).toBe(true)
+   * expect(pipe('1920-01-01', Decoder.validate(birthdayDecoder), Result.isKo)).toBe(true)
+   * ```
+   */
   parse,
+
+  /**
+   * @description
+   * Add a custom validation function, returning either:
+   * - A `DecodeError` if the input is incorrect
+   * - `undefined` if there is no error to report.
+   *
+   * This function gives more control about the returned error than `Decoder.filter` or `Decoder.reject`, but does not allow the value to be modified.
+   *
+   * @see `Decoder.parse`
+   * @see `Decoder.filter`
+   * @see `Decoder.reject`
+   *
+   * @example
+   * ```ts
+   * const validateAge = (dob: string): Option<DecodeError> => {
+   *   const now = new Date()
+   *   const date = new Date(dob)
+   *
+   *   if (date.getFullYear() < now.getFullYear() - 100) {
+   *     return DecodeError.value(dob, 'Date of birth is more than 100 years ago')
+   *   }
+   *   if (date.getFullYear() > now.getFullYear() - 18) {
+   *     return DecodeError.value(dob, 'Date of birth is less than 18 years ago')
+   *   }
+   *   return undefined
+   * }
+   *
+   * const birthdayDecoder = pipe(
+   *   DateDecoder.date,
+   *   Decoder.guard(validateAge)
+   * )
+   *
+   * expect(pipe('1930-01-01', Decoder.validate(birthdayDecoder), Result.isOk)).toBe(true)
+   * expect(pipe('1920-01-01', Decoder.validate(birthdayDecoder), Result.isKo)).toBe(true)
+   * ```
+   */
   guard,
-  nullable,
-  optional,
+
+  /**
+   * @description
+   * Add a `Predicate` filter function to a `Decoder`.
+   * If the value matches the `Predicate`, the value is kept.
+   * Else, an DecodeError with the given message is returned.
+   *
+   * @see `Decoder.parse`
+   * @see `Decoder.guard`
+   * @see `Decoder.reject`
+   *
+   * @example
+   * ```ts
+   * const maxAge = (age: number) => (dob: string) => {
+   *   const now = new Date()
+   *   const date = new Date(dob)
+   *   return date.getFullYear() > now.getFullYear() - age
+   * }
+   * const minAge = (age: number) => (dob: string) => {
+   *   const now = new Date()
+   *   const date = new Date(dob)
+   *   return date.getFullYear() < now.getFullYear() - age
+   * }
+   *
+   * const birthdayDecoder = pipe(
+   *   DateDecoder.date,
+   *   Decoder.filter(maxAge(100), `Date of birth is more than 100 years ago`),
+   *   Decoder.filter(minAge(18), `Date of birth is less than 18 years ago`)
+   * )
+   *
+   * expect(pipe('1930-01-01', Decoder.validate(birthdayDecoder), Result.isOk)).toBe(true)
+   * expect(pipe('1920-01-01', Decoder.validate(birthdayDecoder), Result.isKo)).toBe(true)
+   * ```
+   */
   filter,
+
+  /**
+   * @description
+   * Add a `Predicate` filter function to a `Decoder`.
+   * If the value matches the `Predicate`, an DecodeError with the given message is returned.
+   * Else, the value is kept.
+   *
+   * @see `Decoder.parse`
+   * @see `Decoder.guard`
+   * @see `Decoder.filter`
+   *
+   * @example
+   * ```ts
+   * const decoder = pipe(
+   *   TextDecoder.string,
+   *   Decoder.reject(str => str.length === 0, `string should not be empty`)
+   * )
+   *
+   * expect(pipe('Hello', Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe('', Decoder.validate(decoder), Result.isKo)).toBe(true)
+   * ```
+   */
   reject,
+
+  /**
+   * @description
+   * Makes the value nullable.
+   *
+   * **Note**: If you want to transform an empty string to `null`, use `TextDecoder.nullable` instead.
+   *
+   * @see `Decoder.optional`
+   * @see `TextDecoder.nullable`
+   *
+   * @example
+   * ```ts
+   * const decoder = pipe(
+   *   TextDecoder.string,
+   *   Decoder.nullable
+   * )
+   *
+   * expect(pipe('Hello', Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe('', Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe('', Decoder.validate(decoder), Result.get)).toBe('')
+   * expect(pipe(null, Decoder.validate(decoder), Result.get)).toBe(null)
+   * ```
+   */
+  nullable,
+
+  /**
+   * @description
+   * Makes the value optional (allows `undefined`).
+   *
+   * **Note**: If you want to transform an empty string to `undefined`, use `TextDecoder.optional` instead.
+   *
+   * @see `Decoder.nullable`
+   * @see `TextDecoder.optional`
+   *
+   * @example
+   * ```ts
+   * const decoder = pipe(
+   *   TextDecoder.string,
+   *   Decoder.optional
+   * )
+   *
+   * expect(pipe('Hello', Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe('', Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe('', Decoder.validate(decoder), Result.get)).toBe('')
+   * expect(pipe(undefined, Decoder.validate(decoder), Result.get)).toBe(undefined)
+   * ```
+   */
+  optional,
+
+  /**
+   * @description
+   * This function allows the creation of recursive type decoders.
+   *
+   * @example
+   * ```ts
+   * interface Tree<T> {
+   *   value: T
+   *   forest: Tree<T>[]
+   * }
+   *
+   * // Recursive types require manual typing
+   * const TreeDecoder = <O>(decoder: Decoder<unknown, O>): Decoder<unknown, Tree<O>> =>
+   *   Decoder.lazy(() =>
+   *     ObjectDecoder.struct({
+   *       value: decoder,
+   *       forest: ArrayDecoder.array(TreeDecoder(decoder))
+   *     })
+   *   )
+   *
+   * const input: unknown = {
+   *   value: 'Hello',
+   *   forest: [
+   *     {
+   *        value: 'World',
+   *        forest: []
+   *     }
+   *   ]
+   * }
+   *
+   * expect(pipe(input, TreeDecoder(TextDecoder.string), Result.isOk)).toBe(true)
+   * ```
+   */
   lazy,
+
+  /**
+   * @description
+   * Creates a union `Decoder` that tries, in the given order, if the input is valid.
+   *
+   * @example
+   * ```ts
+   * const decoder = Decoder.union(
+   *   NumberDecoder.number,
+   *   NumberDecoder.fromString
+   * )
+   *
+   * expect(pipe(42, Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe("42", Decoder.validate(decoder), Result.isOk)).toBe(true)
+   * expect(pipe("Hello", Decoder.validate(decoder), Result.isKo)).toBe(true)
+   * ```
+   */
   union,
+
+  /**
+   * @description
+   * Utility to bind a given generated type to a `Decoder`
+   */
   ref,
+
+  /**
+   * @description
+   * Validate an input by the given `Decoder`
+   *
+   * @example
+   * ```
+   * const result = pipe(
+   *   input,
+   *   Decoder.validate(TextDecoder.string)
+   * )
+   *
+   * if (Result.isKo(result)) {
+   *   console.log(result.ko)
+   *   return
+   * }
+   *
+   * const value = result.ok
+   * console.log(value)
+   * ```
+   */
+  validate,
+
+  /**
+   * @description
+   * `Decoder` for an unknown value
+   */
   unknown
 }
