b911a3dfc2ff9d958055f1bef626fa0fbc934a7b
Julien Capellari <julien.capellari@gmail.com>
2021-05-22 11:07:44 +0200

    add(react): useInterval and usePrevious

---
+++ packages/react/src/useInterval.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * useInterval hook
+ * Force the component to render regularly
+ *
+ * @param ms: milliseconds to wait before re-rendering the component
+ * @returns: count of render since the component is mounted
+ */
+function useInterval(ms: number): number {
+  // State
+  const [count, setCount] = useState(0)
+
+  // Effects
+  useEffect(() => {
+    const interval = setInterval(() => setCount((old) => old + 1), ms)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [ms])
+
+  return count
+}
+
+export default useInterval
---
+++ packages/react/src/usePrevious.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react'
+
+/**
+ * usePrevious hook
+ * Returns the previous value of its value parameter
+ *
+ * @param value: value to save
+ * @returns: the previous given value (or null for the 1st render)
+ */
+function usePrevious<T>(value: T): T | null {
+  // Ref
+  const previous = useRef<T | null>(null)
+
+  // Effect
+  useEffect(() => {
+    previous.current = value
+  }, [value])
+
+  return previous.current
+}
+
+export default usePrevious
