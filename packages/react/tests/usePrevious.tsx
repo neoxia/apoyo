21011c492d16c0eb6ef7e9eb630dec6c712958ff
Julien Capellari <julien.capellari@gmail.com>
2021-05-29 17:06:23 +0200

    update(react): test usePrevious hook

--- packages/react/tests/usePrevious.tsx
+++ packages/react/tests/usePrevious.tsx
@@ -1,21 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { render, screen } from '@testing-library/react'
 
 import { usePrevious } from '../src'
 
 // Tests
 describe('usePrevious', () => {
-  it('should return null', () => {
+  it('should return previous value', () => {
     const Test: FC = () => {
-      const previous = usePrevious('test')
+      const [count, setCount] = useState(1)
+      const previous = usePrevious(count)
 
-      return <p>previous: "{previous}"</p>
+      return (
+        <>
+          <button onClick={() => setCount((old) => old + 1)}>increment</button>
+          <p>
+            "{previous}" "{count}"
+          </p>
+        </>
+      )
     }
 
     // Render
     render(<Test />)
 
+    const btn = screen.getByText('increment')
+
     // Checks
-    expect(screen.getByText('previous: ""')).toBeInTheDocument()
+    expect(screen.getByText('"" "1"')).toBeInTheDocument()
+
+    // Increment
+    btn.click()
+    expect(screen.getByText('"1" "2"')).toBeInTheDocument()
+
+    btn.click()
+    expect(screen.getByText('"2" "3"')).toBeInTheDocument()
   })
 })
