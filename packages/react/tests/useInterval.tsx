c4e55f77476e75c4928524cef35f3e874633ffe0
Julien Capellari <julien.capellari@gmail.com>
2021-05-29 17:19:48 +0200

    update(react): test useInterval hook

---
+++ packages/react/tests/useInterval.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react'
+import { render, screen, act } from '@testing-library/react'
+
+import { useInterval } from '../src'
+
+// Test suite
+describe('useInterval', () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  // Tests
+  it('should return previous value', () => {
+    const Test: FC = () => {
+      const count = useInterval(1000)
+
+      return <p>"{count}"</p>
+    }
+
+    // Render
+    render(<Test />)
+
+    // Checks
+    expect(screen.getByText('"0"')).toBeInTheDocument()
+    expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 1000)
+
+    // Advance by 1 second
+    act(() => {
+      jest.advanceTimersByTime(1000)
+    })
+
+    expect(screen.getByText('"1"')).toBeInTheDocument()
+  })
+})
