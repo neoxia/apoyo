38b438c4b4d7ba31b6b9e1c6c8ab0ef2179c10e5
Sascha Braun <sascha.braun245@gmail.com>
2021-10-23 17:24:54 +0200

    feat(scopes): update benchmarks

--- examples/scopes/benchmarks/scopes.js
+++ examples/scopes/benchmarks/scopes.js
@@ -1,43 +1,63 @@
 // @ts-check
 /* eslint-disable */
 
-const Benchmark = require('benchmark')
 const { Scope, Var } = require('@apoyo/scopes')
 const { pipe } = require('@apoyo/std')
 
-const suite = new Benchmark.Suite()
+const double = (value) => value * 2
 
 const scope = pipe(Scope.create(), Scope.get)
+const cached = Var.of(2)
 
-const cached = Var.of({
-  value: 2
-})
+const range = Array(1_000_000)
+  .fill(0)
+  .map((_, idx) => idx)
 
-suite
-  .add('Primitive', () => {
-    return {
-      value: 2
+const benchmark = async (name, fn) => {
+  console.log(`${name} started`)
+  const start = Date.now()
+
+  await fn()
+
+  const end = Date.now()
+  console.log(`${name} done: ${end - start} ms`)
+}
+
+const main = async () => {
+  await benchmark('Primitive', async () => {
+    const res = []
+    for (let item of range) {
+      res.push(pipe(item, double, double, double, double))
+    }
+    console.log('res', res.length)
+  })
+
+  await benchmark('Promise', async () => {
+    const res = []
+    for (let item of range) {
+      res.push(
+        await Promise.resolve(item)
+          // .then((i) => (i % 10000 === 0 ? Prom.sleep(100).then(() => i) : i))
+          .then(double)
+          .then(double)
+          .then(double)
+          .then(double)
+      )
+    }
+    console.log('res', res.length)
+  })
+
+  await benchmark('Var cached', async () => {
+    for (const _item of range) {
+      await scope.get(cached)
     }
   })
-  .add('Promise', () => {
-    return Promise.resolve({
-      value: 2
-    })
+
+  await benchmark('Var unreferenced', async () => {
+    for (const item of range) {
+      await scope.get(Var.of(item))
+    }
   })
-  .add('Var cached', () => {
-    return scope.get(cached)
-  })
-  .add('Var unreferenced', () => {
-    return scope.get(
-      Var.of({
-        value: 2
-      })
-    )
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target))
-  })
-  .on('complete', function () {
-    console.log('Fastest is ' + this.filter('fastest').map('name'))
-  })
-  .run({ async: true })
+}
+
+main()
