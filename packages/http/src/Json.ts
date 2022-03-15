ae1dc1938567d85ec52fd8194cd85ddba97d64b6
Sascha Braun <sascha.braun245@gmail.com>
2022-03-15 21:20:08 +0100

    fix(http): object interfaces not matching json type (#71)

--- packages/http/src/Json.ts
+++
@@ -1,5 +0,0 @@
-export interface JsonMap {
-  [member: string]: string | number | boolean | null | JsonArray | JsonMap
-}
-export interface JsonArray extends Array<string | number | boolean | null | JsonArray | JsonMap> {}
-export type Json = JsonMap | JsonArray | string | number | boolean | null
