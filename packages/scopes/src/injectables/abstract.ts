5e918edfa90c68f6db2cadc79ef2434b2e3a1a69
Sascha Braun <sascha.braun245@gmail.com>
2022-06-25 22:01:21 +0200

    feat(scopes): fix docs + fix missing default value for abstract

--- packages/scopes/src/injectables/abstract.ts
+++ packages/scopes/src/injectables/abstract.ts
@@ -2,8 +2,11 @@ import { Err } from '@apoyo/std'
 import { Injectable } from './injectable'
 
 export class Abstract<T> extends Injectable<T> {
-  constructor(public readonly name: string) {
-    super(() => {
+  constructor(public readonly name: string, public readonly defaultValue?: Injectable<T>) {
+    super((container) => {
+      if (defaultValue) {
+        return container.get(defaultValue)
+      }
       throw Err.of(`Abstract ${JSON.stringify(name)} has not been implemented`, {
         name: 'InjectionError',
         code: 'NotImplemented'
