4c7c585f47d9c706b1339745c607a24318e8c561
Sascha Braun <sascha.braun245@gmail.com>
2022-11-08 17:07:08 +0100

    chore: delete outdated and unnecessary examples in favor of vuepress docs

--- examples/scopes/src/app/features/todos/get-todo.ts
+++
@@ -1,7 +0,0 @@
-import { Todo } from '@/app/models/todo.model'
-import { TodoRepository } from '@/app/repositories/todo.repository'
-import { Injectable } from '@apoyo/scopes'
-
-export const $getTodo = Injectable.define(TodoRepository.$findById, (findById) => {
-  return (id: string): Promise<Todo | undefined> => findById(id)
-})
