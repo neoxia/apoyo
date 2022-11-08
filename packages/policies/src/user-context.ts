c0131473cb3b320e791ceb58d91a45d84b31676a
Sascha Braun <sascha.braun245@gmail.com>
2022-11-08 09:51:05 +0100

    feat(policies): improve user context for easier integration with third-party libraries (#103)

--- packages/policies/src/user-context.ts
+++ packages/policies/src/user-context.ts
@@ -1,11 +1,61 @@
 import { AsyncLocalStorage } from 'async_hooks'
-import { NotAuthenticatedException } from './exceptions'
+import { NotAuthenticatedException, NoUserContextException } from './exceptions'
 
 export class UserContext<User> {
-  private readonly _als = new AsyncLocalStorage<User | null>()
+  private readonly _als = new AsyncLocalStorage<{ user: User | null }>()
 
+  /**
+   * Create a new user context for the given asynchroneous scope.
+   *
+   * Once a context has been created, you will be able to set data in your user context inside the asynchroneous scope.
+   *
+   * @see `UserContext#setUser`
+   */
+  public createContext<T>(fn: () => T) {
+    const user = null
+    return this._als.run({ user }, fn)
+  }
+
+  /**
+   * Set the current user in the current user context.
+   * If no user context is initialized, this function will throw an `NoUserContextException`.
+   *
+   * @example
+   * ```
+   * userContext.createContext(async () => {
+   *  // Set user in current context
+   *  userContext.setUser(myUser)
+   *
+   *  // Get user in current context
+   *  userContext.getCurrentUser()
+   * })
+   *
+   * // This will throw an `NoUserContextException` exception:
+   * userContext.setUser(myUser)
+   * ```
+   */
+  public setUser(user: User | null) {
+    const store = this._als.getStore() ?? null
+    if (!store) {
+      throw new NoUserContextException()
+    }
+    store.user = user
+  }
+
+  /**
+   * Create a new user context for the given asynchroneous scope.
+   *
+   * This is the same as:
+   *
+   * ```ts
+   * userContext.createContext(() => {
+   *   userContext.setUser(user)
+   *   return fn()
+   * })
+   * ```
+   */
   public forUser<T>(user: User | null, fn: () => T) {
-    return this._als.run(user, fn)
+    return this._als.run({ user }, fn)
   }
 
   public getCurrentUser(): User
@@ -13,7 +63,7 @@ export class UserContext<User> {
   public getCurrentUser(options: { allowGuest: true }): User | null
   public getCurrentUser(options?: { allowGuest: boolean }): User | null {
     const allowGuest = options?.allowGuest ?? false
-    const user = this._als.getStore() ?? null
+    const user = this._als.getStore()?.user ?? null
     if (!allowGuest && !user) {
       throw new NotAuthenticatedException()
     }
--- packages/policies/tests/user-context.spec.ts
+++ packages/policies/tests/user-context.spec.ts
@@ -1,7 +1,97 @@
-import { NotAuthenticatedException, UserContext } from '../src'
+import { Prom } from '@apoyo/std'
+import { NotAuthenticatedException, NoUserContextException, UserContext } from '../src'
 import { User } from './setup/types'
 
 describe('UserContext', () => {
+  it('should create new user context', async () => {
+    const userContext = new UserContext<User>()
+
+    const user: User = {
+      id: 'user_id_1',
+      email: 'test@example.com',
+      role: 'admin'
+    }
+
+    await userContext.createContext(async () => {
+      userContext.setUser(user)
+
+      expect(userContext.getCurrentUser()).toEqual(user)
+    })
+  })
+
+  it('should be able to set user in user context correctly', async () => {
+    const userContext = new UserContext<User>()
+
+    const user: User = {
+      id: 'user_id_1',
+      email: 'test@example.com',
+      role: 'admin'
+    }
+
+    async function auth() {
+      // Fetching our user takes some time...
+      await Prom.sleep(10)
+      // Set user
+      userContext.setUser(user)
+    }
+
+    await userContext.createContext(async () => {
+      await auth()
+
+      expect(userContext.getCurrentUser()).toEqual(user)
+    })
+  })
+
+  it('should throw when trying to set user outside of user context', async () => {
+    const userContext = new UserContext<User>()
+
+    const user: User = {
+      id: 'user_id_1',
+      email: 'test@example.com',
+      role: 'admin'
+    }
+
+    async function auth() {
+      // Fetching our user takes some time...
+      await Prom.sleep(10)
+      // Set user
+      userContext.setUser(user)
+    }
+
+    expect(auth()).rejects.toThrow(NoUserContextException)
+  })
+
+  it('should be able to create sub-context', async () => {
+    const userContext = new UserContext<User>()
+
+    const user1: User = {
+      id: 'user_id_1',
+      email: 'test@example.com',
+      role: 'admin'
+    }
+    const user2: User = {
+      id: 'user_id_2',
+      email: 'test@example.com',
+      role: 'admin'
+    }
+
+    await userContext.createContext(async () => {
+      userContext.setUser(user1)
+
+      await userContext.createContext(async () => {
+        userContext.setUser(user2)
+
+        expect(userContext.getCurrentUser()).toEqual(user2)
+      })
+
+      await userContext.forUser(user2, async () => {
+        expect(userContext.getCurrentUser()).toEqual(user2)
+      })
+
+      expect(userContext.getCurrentUser()).toEqual(user1)
+    })
+  })
+
   it('should store current user in async context', async () => {
     const userContext = new UserContext<User>()
 
