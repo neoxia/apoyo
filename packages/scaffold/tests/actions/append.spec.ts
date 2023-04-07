3df92655edc52fa104e3fa4aecc20cb293c99c58
Sascha Braun <sascha.braun245@gmail.com>
2023-04-07 09:43:50 +0200

    feat(scaffold): improve append and prepend injection actions

--- packages/scaffold/tests/actions/append.spec.ts
+++ packages/scaffold/tests/actions/append.spec.ts
@@ -25,5 +25,67 @@ describe('AppendAction', () => {
         console.log('After');"
       `)
     })
+
+    it('should execute action correctly with inline content', async () => {
+      await scaffolder.destination.write('hello-john.ts', `console.log('Hello John');`)
+
+      const action = new AppendAction({
+        from: {
+          content: `console.log('Goodbye <%= name %>');`
+        },
+        to: 'hello-<%= h.changeCase.paramCase(name) %>.ts',
+        after: `console.log('Hello <%= name %>')`,
+        parameters: {
+          name: 'John'
+        }
+      })
+
+      await action.execute(scaffolder)
+
+      expect(await scaffolder.destination.get('hello-john.ts')).toMatchInlineSnapshot(`
+        "console.log('Hello John');
+        console.log('Goodbye John');"
+      `)
+    })
+
+    it('should append at the end of the file without after property', async () => {
+      await scaffolder.destination.write('hello-john.ts', `console.log('Hello John');`)
+
+      const action = new AppendAction({
+        from: {
+          content: `console.log('Goodbye <%= name %>');`
+        },
+        to: 'hello-<%= h.changeCase.paramCase(name) %>.ts',
+        parameters: {
+          name: 'John'
+        }
+      })
+
+      await action.execute(scaffolder)
+
+      expect(await scaffolder.destination.get('hello-john.ts')).toMatchInlineSnapshot(`
+        "console.log('Hello John');
+        console.log('Goodbye John');"
+      `)
+    })
+
+    it('should skip action if content already exists', async () => {
+      await scaffolder.destination.write('hello-john.ts', `console.log('Hello John');`)
+
+      const action = new AppendAction({
+        from: {
+          content: `console.log('Hello <%= name %>');`
+        },
+        to: 'hello-<%= h.changeCase.paramCase(name) %>.ts',
+        skipIf: 'Hello <%= name %>',
+        parameters: {
+          name: 'John'
+        }
+      })
+
+      await action.execute(scaffolder)
+
+      expect(await scaffolder.destination.get('hello-john.ts')).toMatchInlineSnapshot(`"console.log('Hello John');"`)
+    })
   })
 })
--- packages/scaffold/tests/actions/prepend.spec.ts
+++ packages/scaffold/tests/actions/prepend.spec.ts
@@ -25,5 +25,67 @@ describe('PrependAction', () => {
         console.log('Hello John');"
       `)
     })
+
+    it('should execute action correctly with inline content', async () => {
+      await scaffolder.destination.write('hello-john.ts', `console.log('Hello John');`)
+
+      const action = new PrependAction({
+        from: {
+          content: `console.log('<%= name %> enters the house');`
+        },
+        to: 'hello-<%= h.changeCase.paramCase(name) %>.ts',
+        before: `console.log('Hello <%= name %>')`,
+        parameters: {
+          name: 'John'
+        }
+      })
+
+      await action.execute(scaffolder)
+
+      expect(await scaffolder.destination.get('hello-john.ts')).toMatchInlineSnapshot(`
+        "console.log('John enters the house');
+        console.log('Hello John');"
+      `)
+    })
+
+    it('should prepend at the top of the file without before property', async () => {
+      await scaffolder.destination.write('hello-john.ts', `console.log('Hello John');`)
+
+      const action = new PrependAction({
+        from: {
+          content: `console.log('<%= name %> enters the house');`
+        },
+        to: 'hello-<%= h.changeCase.paramCase(name) %>.ts',
+        parameters: {
+          name: 'John'
+        }
+      })
+
+      await action.execute(scaffolder)
+
+      expect(await scaffolder.destination.get('hello-john.ts')).toMatchInlineSnapshot(`
+        "console.log('John enters the house');
+        console.log('Hello John');"
+      `)
+    })
+
+    it('should skip action if content already exists', async () => {
+      await scaffolder.destination.write('hello-john.ts', `console.log('Hello John');`)
+
+      const action = new PrependAction({
+        from: {
+          content: `console.log('Hello <%= name %>');`
+        },
+        to: 'hello-<%= h.changeCase.paramCase(name) %>.ts',
+        skipIf: 'Hello <%= name %>',
+        parameters: {
+          name: 'John'
+        }
+      })
+
+      await action.execute(scaffolder)
+
+      expect(await scaffolder.destination.get('hello-john.ts')).toMatchInlineSnapshot(`"console.log('Hello John');"`)
+    })
   })
 })
